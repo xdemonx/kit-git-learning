84bcae740343e2ff3c0834fb766b7be26168df09
alexandr1995 <alexandr1995@users.noreply.github.com>
2016-05-14 10:55:57 +0500

    ROLF

--- src/main/java/psu/kit/tanya/TanyaCalculator.java
+++ src/main/java/psu/kit/tanya/TanyaCalculator.java
@@ -4,10 +4,56 @@ import java.util.Scanner;
 
 public class TanyaCalculator {
 
+    int[] countOfCharacters = new int[256];
+    boolean[] used;
 
     public TanyaReaction calculate(String s, String t) {
 
-        throw new UnsupportedOperationException("Метод ещё не реалиозован");
+ used = new boolean[s.length()];
 
+        int countYay = tanyaYayCount(s, t);
+        int countWhoops = tanyaWhoopsCount(s, t);
+
+        return new TanyaReaction(countYay, countWhoops);
+    }
+
+    private int tanyaYayCount(String s, String t) {
+
+        int countYay = 0;
+
+        for (int i = 0; i < t.length(); i++) {
+ countOfCharacters[t.charAt(i)] = countOfCharacters[t.charAt(i)] + 1;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (countOfCharacters[s.charAt(i)] != 0) {
+ countYay++;
+ countOfCharacters[s.charAt(i)]--;
+ used[i] = true;
+            }
+        }
+        return countYay;
+    }
+
+    private int tanyaWhoopsCount(String s, String t) {
+
+        char sStringSymbol;
+
+        int countWhoops = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (!used[i]) {
+ sStringSymbol = s.charAt(i);
+                if (sStringSymbol <= 'Z') {
+ sStringSymbol = Character.toLowerCase(sStringSymbol);
+ } else {
+ sStringSymbol = Character.toUpperCase(sStringSymbol);
+                }
+                if (countOfCharacters[sStringSymbol] != 0) {
+ countOfCharacters[sStringSymbol]--;
+ countWhoops++;
+                }
+            }
+        }
+        return countWhoops;
     }
 }
