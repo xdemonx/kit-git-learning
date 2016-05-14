2a07f38b03e1077dc848311276d4f46d5cfa782c
DimasMaracas <gektor775@yandex.ru>
2016-05-14 21:11:51 +0500

    1
    Add

--- src/main/java/psu/kit/tanya/TanyaCalculator.java
+++ src/main/java/psu/kit/tanya/TanyaCalculator.java
@@ -1,13 +1,37 @@
 package psu.kit.tanya;
 
-import java.util.Scanner;
-
 public class TanyaCalculator {
-
-
     public TanyaReaction calculate(String s, String t) {
+        int[] tArray = new int[255];
+        String sCopy = s;
+        StringBuilder tmp = new StringBuilder();
+        int yay, whoops;
 
-        throw new UnsupportedOperationException("Метод ещё не реалиозован");
+        for (int i = 0; i < t.length(); i++) {
+            tArray[t.charAt(i)]++;
+        }
 
+        yay = countReaction(sCopy, tArray, tmp) ;
+        sCopy = tmp.toString();
+        whoops = countReaction(sCopy, tArray, tmp);
+
+        return new TanyaReaction(yay, whoops);
     }
+
+    private int countReaction(String s, int[] tArray, StringBuilder reversedLastCharacters) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (tArray[s.charAt(i)]  0) {
+                count++;
+                tArray[s.charAt(i)]--;
+            } else {
+                reversedLastCharacters.append(
+                        (Character.isUpperCase(s.charAt(i)))
+                        Character.toLowerCase(s.charAt(i))
+                        Character.toUpperCase(s.charAt(i)));
+            }
+        }
+        return count;
+    }
+
 }
