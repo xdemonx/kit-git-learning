7668b1d2a3399bd7caa83bc8815a2d875d8c4525
feyawinx <cancell59@mail.ru>
2016-05-18 01:24:35 +0400

    “omg”

--- src/main/java/psu/kit/tanya/TanyaCalculator.java
+++ src/main/java/psu/kit/tanya/TanyaCalculator.java
@@ -1,13 +1,58 @@
 package psu.kit.tanya;
 
-import java.util.Scanner;
-
 public class TanyaCalculator {
 
+    final static int ALPHABET_SIZE = 26;
 
     public TanyaReaction calculate(String s, String t) {
 
-        throw new UnsupportedOperationException("Метод ещё не реалиозован");
+        int whoopsCount = 0;
+        int yayCount = 0;
 
+        int[] countT = new int[ALPHABET_SIZE * 2];
+        int[] countS = new int[ALPHABET_SIZE * 2];
+
+        countLetters(countS, s);
+        countLetters(countT, t);
+
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            while (countS[i] > 0 && countT[i] > 0) {
+                yayCount++;
+                countS[i]--;
+                countT[i]--;
+            }
+            while (countS[i + ALPHABET_SIZE] > 0 && countT[i + ALPHABET_SIZE] > 0) {
+                yayCount++;
+                countS[i + ALPHABET_SIZE]--;
+                countT[i + ALPHABET_SIZE]--;
+            }
+            while (countS[i] > 0 && countT[i + ALPHABET_SIZE] > 0) {
+                whoopsCount++;
+                countS[i]--;
+                countT[i + ALPHABET_SIZE]--;
+            }
+            while (countS[i + ALPHABET_SIZE] > 0 && countT[i] > 0) {
+                whoopsCount++;
+                countS[i + ALPHABET_SIZE]--;
+                countT[i]--;
+            }
+        }
+
+        return new TanyaReaction(yayCount, whoopsCount);
     }
+
+    private int convertLetterToIndex(char c) {
+        if (Character.isUpperCase(c)) {
+            return c - 'A';
+        } else {
+            return c - 'a' + ALPHABET_SIZE;
+        }
+    }
+
+    private void countLetters(int[] a, String s) {
+        for (int i = 0; i < s.length(); i++) {
+            a[convertLetterToIndex(s.charAt(i))]++;
+        }
+    }
+
 }
