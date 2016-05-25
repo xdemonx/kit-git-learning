ed5a1ac745b2bc5a880f6673db046f69a99fc934
DimasMaracas <gektor775@yandex.ru>
2016-05-25 17:28:41 +0500

    Correct

--- src/main/java/psu/kit/tanya/TanyaCalculator.java
+++ src/main/java/psu/kit/tanya/TanyaCalculator.java
@@ -1,13 +1,38 @@
 package psu.kit.tanya;
 
-import java.util.Scanner;
-
 public class TanyaCalculator {
+        public TanyaReaction calculate(String s, String t) {
+                int[] tArray = new int[255];
+                String sCopy = s;
+                StringBuilder tmp = new StringBuilder();
+                int yay, whoops;
 
+                for (int i = 0; i < t.length(); i++) {
+                        tArray[t.charAt(i)]++;
+                    }
 
-    public TanyaReaction calculate(String s, String t) {
+            public TanyaReaction calculate(String s, String t) {
+                    yay = countReaction(sCopy, tArray, tmp) ;
+                    sCopy = tmp.toString();
+                    whoops = countReaction(sCopy, tArray, tmp);
 
-        throw new UnsupportedOperationException("Метод ещё не реалиозован");
+                    return new TanyaReaction(yay, whoops);
+                }
 
+            private int countReaction(String s, int[] tArray, StringBuilder reversedLastCharacters) {
+                    int count = 0;
+                    for (int i = 0; i < s.length(); i++) {
+                            if (tArray[s.charAt(i)]  0) {
+                                    count++;
+                                    tArray[s.charAt(i)]--;
+                                } else {
+                                    reversedLastCharacters.append(
+                                                    (Character.isUpperCase(s.charAt(i)))
+                                                                    Character.toLowerCase(s.charAt(i))
+                                                                    Character.toUpperCase(s.charAt(i)));
+                                }
+                        }
+                    return count;
+        }
+        
     }
-}
